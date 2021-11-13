d6277e43940e4db8ffc62c90b36c0506b94710c1
Stephanie Mori <cathsteph93@gmail.com>
2021-11-13 14:01:30 -0500

    fixes made to isEnd file - added use of winning streak to find win

--- skeleton-LineEmUp.py
+++ skeleton-LineEmUp.py
@@ -110,40 +110,8 @@ class Game:
 		else:
 			return True
 
-	def is_end(self): # IN OTHER FILE - is_end().py
-		# Vertical win
-		for i in range(0, 3):
-			if (self.current_state[0][i] != '.' and
-				self.current_state[0][i] == self.current_state[1][i] and
-				self.current_state[1][i] == self.current_state[2][i]):
-				return self.current_state[0][i]
-		# Horizontal win
-		for i in range(0, 3):
-			if (self.current_state[i] == ['X', 'X', 'X']):
-				return 'X'
-			elif (self.current_state[i] == ['O', 'O', 'O']):
-				return 'O'
-		# Main diagonal win
-		if (self.current_state[0][0] != '.' and
-			self.current_state[0][0] == self.current_state[1][1] and
-			self.current_state[0][0] == self.current_state[2][2]):
-			return self.current_state[0][0]
-		# Second diagonal win
-		if (self.current_state[0][2] != '.' and
-			self.current_state[0][2] == self.current_state[1][1] and
-			self.current_state[0][2] == self.current_state[2][0]):
-			return self.current_state[0][2]
-		# Is whole board full?
-		for i in range(0, 3):
-			for j in range(0, 3):
-				# There's an empty field, we continue the game
-				if (self.current_state[i][j] == '.'):
-					return None
-		# It's a tie!
-		return '.'
-
 	def check_end(self): # works with line em up needs
-		self.result = self.is_end()
+		self.result = isEnd.is_end(self)
 		# Printing the appropriate message if the game has ended
 		if self.result != None:
 			if self.result == 'X':
@@ -184,7 +152,7 @@ class Game:
 			value = -2
 		x = None
 		y = None
-		result = self.is_end()
+		result = isEnd.is_end(self)
 		if result == 'X':
 			return (-1, x, y)
 		elif result == 'O':
@@ -223,7 +191,7 @@ class Game:
 			value = -2
 		x = None
 		y = None
-		result = self.is_end()
+		result = isEnd.is_end(self)
 		if result == 'X':
 			return (-1, x, y)
 		elif result == 'O':
